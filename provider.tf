09e9e5e578e9611b2830ba15bd65886d31d8e12f
OKUNOWO <TEmuh@sdcounty.ca.gov>
2023-11-14 23:15:03 +0100

    updating github workflow

--- provider.tf
+++ provider.tf
@@ -1,4 +1,7 @@
 terraform {
+    backend "s3" {
+
+    }
     required_providers {
         aws = {
       source  = "hashicorp/aws"
@@ -9,8 +12,5 @@ terraform {
 
 provider "aws" {
   region = "us-east-1"
-  access_key = "ASIAVWXV3U5GGTDQGH6D"
-  secret_key = "UfQCehPJNBY1CzjVckmLOYRap7Cw/vTQ5NdE8Lf3"
-  token = "FwoGZXIvYXdzEIb//////////wEaDNoMXQQ19q1Iz8L7jyLJAZ8ghxz7Obi0wTn9Fq4y7jsl887WqUffQFS857hbr7PBJVMVJDcy70/NIF8KYUOijhrQ+UXZtFmrxiIgs6hSOuq05mLE8l6nZdtvEY7462J9w4cxliQ2htOLocfGS2rGuHIpG2zxPeCxwn0blB0MK1T4I6jHgtnJrjhXC/fy7QAidxUSycTf9cxz4T7jBfUqDUif+juyehgL7xboT0lzAge6mexdyhKcWJrRwPgg5MOatY8MyOU+m5YEgNTkE+gcEcOorRZGh8MSbSjmvaqqBjItMpxBni953ywMqQIg982+yzC59E1WCfZYzZA9mHc8OKQnStjMSUvK2TKrEfGR"
   
 }
